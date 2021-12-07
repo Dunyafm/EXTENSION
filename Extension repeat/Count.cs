e4859eb62cf68e21b926754294ec57d20b499ee8
unknown <Dunyafm@code.edu.az>
2021-12-07 15:17:40 -0800

    Extension Repeat

---
+++ Extension repeat/Count.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repeat_extension
+{
+    public static class Count
+    {
+        public static string Quiz(this string n , int num )
+        { 
+              if(num <= 0)
+              {
+                return (n);
+              }
+        } 
+    }
+} 
+   
