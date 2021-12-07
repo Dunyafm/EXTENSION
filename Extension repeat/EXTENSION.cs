934a2457e038f819583b4ee52ea51de358172f41
unknown <Dunyafm@code.edu.az>
2021-12-07 15:13:41 -0800

    Extension Repeat

---
+++ Extension repeat/EXTENSION.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace Repeat_extension
+{
+    public class Program
+    {
+        public static void Test(string count)
+        {
+            var n = "123456";
+
+            Console.WriteLine("n".Repeat(5));
+
+        }
+        
+    }
+
+  
+}
+    
+     
+    
+
